67474e7f4cc0011f533a5eb65680c826b2ea6827
cuppajoeman <ccn@cuppajoeman.com>
2024-05-04 03:01:58 -0400

    add in delta time measurements

--- rate_limited_loop.cpp
+++ rate_limited_loop.cpp
@@ -8,23 +8,34 @@
  * \note you probably want to run this in it's own thread so the sleep doesn't
  * pause your whole program
  */
-void RateLimitedLoop::start(
-    double update_rate_hz, const std::function<void()> &rate_limited_func,
-    const std::function<bool()> &termination_condition_func) {
+void RateLimitedLoop::start(double update_rate_hz, const std::function<void(double)> &rate_limited_func,
+                            const std::function<bool()> &termination_condition_func) {
 
-  // 1/N seconds per iteration
-  std::chrono::duration<double> update_period(1.0 / update_rate_hz);
+    // 1/N seconds per iteration
+    std::chrono::duration<double> update_period(1.0 / update_rate_hz);
 
-  while (!termination_condition_func()) {
-    rate_limited_func();
+    bool first_iteration = true;
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> previous_time;
 
-    std::chrono::time_point<std::chrono::steady_clock,
-                            std::chrono::duration<double>>
-        now = std::chrono::steady_clock::now();
-    std::chrono::time_point<std::chrono::steady_clock,
-                            std::chrono::duration<double>>
-        wake_time = now + update_period;
+    while (!termination_condition_func()) {
 
-    std::this_thread::sleep_until(wake_time);
-  }
+        if (first_iteration) {
+            previous_time = std::chrono::steady_clock::now();
+            first_iteration = false;
+        } else {
+            std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> current_time =
+                std::chrono::steady_clock::now();
+
+            std::chrono::duration<double> delta = current_time - previous_time;
+
+            rate_limited_func(delta.count());
+
+            std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> wake_time =
+                current_time + update_period;
+
+            previous_time = current_time;
+
+            std::this_thread::sleep_until(wake_time);
+        }
+    }
 }
--- rate_limited_loop.hpp
+++ rate_limited_loop.hpp
@@ -4,10 +4,9 @@
 #include <functional>
 
 class RateLimitedLoop {
-public:
-  void start(double update_rate_hz,
-             const std::function<void()> &rate_limited_func,
-             const std::function<bool()> &termination_condition_func);
+  public:
+    void start(double update_rate_hz, const std::function<void(double)> &rate_limited_func,
+               const std::function<bool()> &termination_condition_func);
 };
 
 #endif // RATE_LIMITED_LOOP_HPP

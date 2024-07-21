8a2579f4aedc65e6dd59ca97fe2e79f5823bd8b4
cuppajoeman <ccn@cuppajoeman.com>
2024-07-21 02:59:27 -0400

    use sbpt

--- fixed_frequency_loop.hpp
+++ fixed_frequency_loop.hpp
@@ -2,12 +2,13 @@
 #define RATE_LIMITED_LOOP_HPP
 
 #include <functional>
-#include "stopwatch/stopwatch.hpp"
+#include "sbpt_generated_includes.hpp"
 
 class FixedFrequencyLoop {
-  public:
+public:
     void start(double update_rate_hz, const std::function<void(double)> &rate_limited_func,
                const std::function<bool()> &termination_condition_func);
+
     Stopwatch stopwatch;
 };
 
